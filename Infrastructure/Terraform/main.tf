4201e48b7229326298bf582e484bc77401c52f4f
aminetitrofine <amine.titrofine@gmail.com>
2023-11-20 13:30:52 +0100

    provision infrastructure with terraform

--- Infrastructure/Terraform/main.tf
+++ Infrastructure/Terraform/main.tf
@@ -1,17 +1,82 @@
 resource "google_container_cluster" "my_cluster" {
 
   name     = var.cluster-name
-  location = var.region
-
-  # Enabling autopilot for this cluster
-  enable_autopilot = true
-
-  # Setting an empty ip_allocation_policy to allow autopilot cluster to spin up correctly
+  location = var.zone
+  initial_node_count       = 1
+  remove_default_node_pool = true
   ip_allocation_policy {
   }
+  deletion_protection = false
 
-  # Avoid setting deletion_protection to false
-  # until you're ready (and certain you want) to destroy the cluster.
-  # deletion_protection = false
 
+}
+resource "google_container_node_pool" "primary_nodes" {
+  name       = "${var.cluster-name}-node-pool"
+  location   = var.zone
+  cluster    = var.cluster-name
+  node_count = var.node_count
+
+  management {
+    auto_repair  = true
+    auto_upgrade = true
+  }
+
+  autoscaling {
+    min_node_count = var.minNode
+    max_node_count = var.maxNode
+  }
+
+  timeouts {
+    create = "20m"
+    update = "20m"
+  }
+
+
+  depends_on = [google_container_cluster.my_cluster]
+  node_config {
+    preemptible  = true
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      env = var.project_id
+    }
+
+    machine_type = "e2-standard-2"
+    tags         = ["gke-node", "${var.project_id}-gke"]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+
+
+
+  }
+}
+resource "google_compute_instance" "vm_instance" {
+  name         = "test-machine"
+  machine_type = "e2-standard-2"
+  zone         = var.zone
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+      nat_ip = google_compute_address.external_ip.address
+    }
+  }
+  metadata = {
+    ssh-keys = "${var.vm_ssh_user}:${file(var.vm_ssh_pub_key_file)}"
+  }
+  
+
+}
+resource "google_compute_address" "external_ip" {
+  name = "dynamic-external-ip"
+  address_type = "EXTERNAL"
 }
--- Infrastructure/Terraform/variables.tf
+++ Infrastructure/Terraform/variables.tf
@@ -2,11 +2,15 @@ variable "project_id" {
   type        = string
   description = "The GCP project ID to apply this config to"
 }
-
 variable "region" {
   type        = string
   description = "Region of the infrastructure"
   default     = "us-central1"
+}
+variable "zone" {
+  type        = string
+  description = "zone of the infrastructure"
+  default     = "us-central1-a"
 }
 
 variable "credentials-path" {
@@ -15,4 +19,22 @@ variable "credentials-path" {
 
 variable "cluster-name" {
     default = "online-boutique"
+}
+
+variable "node_count" {
+    default = 1
+}
+
+variable "minNode" {
+    default = 1
+}
+
+variable "maxNode" {
+    default = 3
+}
+variable "vm_ssh_user" {
+    default = "devops-admin"
+}
+variable "vm_ssh_pub_key_file" {
+    default = "./../../keys/.ssh/id_rsa.pub"
 }
